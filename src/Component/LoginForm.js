287b4282417735bacfe8546ec448ddf7d5b3a6dd
NpSingh12 <narinderpals404@gmail.com>
2024-04-23 15:27:42 +0530

    add Login form

---
+++ src/Component/LoginForm.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import "./LoginFormModule.css";
+
+export default function LoginForm() {
+  const submitemail = () => {
+    console.log(email);
+    console.log(password);
+  };
+  const handelemail = (e) => {
+    setemail(e.target.value);
+  };
+  const handelpassword = (e) => {
+    setpassword(e.target.value);
+  };
+
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+
+  return (
+    <div className="login-form-container">
+      <form className="login-form">
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1">Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            value={email}
+            onChange={handelemail}
+          />
+          <div id="emailHelp" className="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+            value={password}
+            onChange={handelpassword}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary" onClick={submitemail}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -1,44 +1,101 @@
-import React from 'react'
+import React, { useState } from "react";
+import LoginForm from "./LoginForm";
 
 export default function Navbar() {
+  const [showLoginForm, setShowLoginForm] = useState(false);
+
+  const toggleLoginForm = () => {
+    setShowLoginForm(!showLoginForm);
+  };
+
   return (
-   <>
-    <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="/">Navbar</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/">Features</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/">Pricing</a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown link
+    <>
+      <nav
+        className="navbar bg-dark border-bottom border-body"
+        data-bs-theme="dark"
+      >
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+            Navbar
           </a>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="/">Action</a></li>
-            <li><a className="dropdown-item" href="/">Another action</a></li>
-            <li><a className="dropdown-item" href="/">Something else here</a></li>
-          </ul>
-        </li>
-      </ul>
-   
-    <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-  </div>
-  </div>
-</nav>
-   </>
-  )
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">
+                  Features
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">
+                  Pricing
+                </a>
+              </li>
+              <li className="nav-item">
+                <button className="nav-link btn" onClick={toggleLoginForm}>
+                  Login
+                </button>
+              </li>
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="/"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Dropdown link
+                </a>
+                <ul className="dropdown-menu">
+                  <li>
+                    <a className="dropdown-item" href="/">
+                      Action
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="/">
+                      Another action
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="/">
+                      Something else here
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+
+            <form className="d-flex" role="search">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
+      {showLoginForm && <LoginForm />}
+    </>
+  );
 }
